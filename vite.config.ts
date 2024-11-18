27587e6b2802556d633d25bbe09f8c0e24b99578
Coordinate-Cat <tetrapasta02@gmail.com>
2024-11-18 12:55:04 +0900

    feat: Update TypeScript configuration and add path aliases; remove unused SVG assets; enhance Tailwind CSS configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+  // shadcn
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
