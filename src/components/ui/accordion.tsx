43416e3e1fed4a1127bb0072db6ff3ac570002f8
Coordinate-Cat <tetrapasta02@gmail.com>
2024-12-12 10:12:48 +0900

    docs: Update CONTRIBUTING.md to reflect changes in branch and types sections; emphasize reading the Code of Conduct before contributing

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -36,11 +36,17 @@ AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
 
 const AccordionContent = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content> & {
+    isVisible: boolean;
+  }
+>(({ className, children, isVisible, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className={cn(
+      "overflow-hidden text-sm",
+      className,
+      isVisible ? "data-[state=open]" : "data-[state=closed]"
+    )}
     {...props}
   >
     <div className={cn("pt-0", className)}>{children}</div>
