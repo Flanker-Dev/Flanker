0696bff57bb3c13372fda0fbdcb27cc744e47cbb
Coordinate-Cat <tetrapasta02@gmail.com>
2024-12-24 18:04:26 +0900

    feat: Add increase_height command to adjust window height dynamically

---
+++ src-tauri/src/increase_height.rs
@@ -0,0 +1,13 @@
+use tauri::Window;
+
+#[tauri::command]
+pub async fn increase_height(window: Window) {
+    tauri::async_runtime::spawn(async move {
+        let current_size = window.outer_size().unwrap();
+        let scale_factor = window.scale_factor().unwrap();
+        window.set_size(tauri::Size::Logical(tauri::LogicalSize {
+            width: current_size.width as f64 / scale_factor,
+            height: current_size.height as f64 / scale_factor + 20.0,
+        })).unwrap();
+    }).await.unwrap();
+}
